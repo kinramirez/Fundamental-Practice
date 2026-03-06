3e88240c4bc1378b19389f5afd2516da7beca4f4
bulsu-ramirezkjm <james.kin.ramirez@gmail.com>
2026-03-06 12:59:43 +0800

    daily practice session

--- src/assets/js/practice.js
+++ src/assets/js/practice.js
@@ -1,11 +1,3 @@
-let printNumber = function () {
-    const output = document.getElementById("output");
-    for (let i = 1; i <= 10; i++) {
-        output.innerHTML += i + "<br>";
-    }
-    output.innerHTML += "End Loop";
-}
-printNumber();
 /*
 Problem: print number 1 to 10
 
@@ -18,3 +10,42 @@ increment after print
 
 print again
 */
+let printNumber = function () {
+    const output = document.getElementById("output");
+    for (let i = 1; i <= 10; i++) {
+        output.innerHTML += i + "<br>";
+    }
+    output.innerHTML += "End Loop";
+}
+//----------------------------------
+/*
+sum two number
+
+solution
+take two number from user
+
+add two number
+
+print result
+*/
+
+let sum = function () {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const result = num1 + num2;
+    document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+//----------------------------------
+
+let isEven = function () {
+    const num = parseInt(document.getElementById("num").value);
+    
+    if (num % 2 === 0) {
+        output3.innerHTML = `${num} is even number.`;
+    }
+    else {
+        output3.innerHTML = `${num} is odd number.`;
+    }
+}
+document.getElementById("check").addEventListener("click", isEven);
